245ed1f7795392feddc8765cffe69cb48dfb4c8a
Jojatekok <jojatekokweb@gmail.com>
2013-10-26 13:06:37 +0200

    Separated time format codes into a new class

---
+++ WPF Stopwatch/TimeFormats.cs
@@ -0,0 +1,8 @@
+﻿namespace WpfStopwatch
+{
+    static class TimeFormats
+    {
+        public const string Default = @"hh\:mm\:ss\.fff";
+        public const string Long = @"d\.hh\:mm\:ss\.fff";
+    }
+}
